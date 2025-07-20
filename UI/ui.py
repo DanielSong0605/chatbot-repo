a00343a379819b850136a33c9264000919d54aef
Daniel <songdaniel0605@gmail.com>
2025-07-20 15:24:20 -0700

    Resized UI

--- UI/ui.py
+++ UI/ui.py
@@ -1,6 +1,8 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtCore import QTimer
+import os
+from PySide6.QtGui import QPixmap, QPainter, QColor, QBrush
+from PySide6.QtCore import QTimer, Qt
 from PySide6.QtWidgets import (
     QApplication,
     QCheckBox,
@@ -38,7 +40,13 @@ class ui(QWidget):
         self.ui = Ui_ui()
         self.ui.setupUi(self)
 
-    #initializes variables
+        # Initial image set
+        img_path = os.path.join(os.path.dirname(__file__), "images/awake.jpg")
+        pixmap = QPixmap(img_path)
+        self.ui.sleepStatus.setScaledContents(True)
+        self.ui.sleepStatus.setPixmap(pixmap)
+
+        #initializes variables
         self.isMuted = False
         self.isDeafened = False
         self.textInput = ""
@@ -46,34 +54,35 @@ class ui(QWidget):
         self.sleep = False
 
         # Connect signals to functions
-        self.ui.mute.toggled.connect(self.toggle_mute)
-        self.ui.deafen.toggled.connect(self.toggle_deafen)
+        self.ui.mute.clicked.connect(self.toggle_mute)
+        self.ui.deafen.clicked.connect(self.toggle_deafen)
         self.ui.sleepMode.clicked.connect(self.sleepModel)
         self.ui.enterButton.clicked.connect(self.getTextInput)
+        self.ui.showBEBox.toggled.connect(self.backEndBox)
 
         #connects to update_sleep_button type shii to checka nd update the text to sleeping/awake every 200ms
         self.timer = QTimer()
-        self.timer.timeout.connect(self.update_sleep_button_text)
+        self.timer.timeout.connect(self.update_buttons)
         self.timer.start(200)
-        
+
     # functions here
+
     # for toggling mute
-    def toggle_mute(self, checked):
-        self.isMuted = checked
-        if checked:
-            self.ui.mute.setText("Unmute")
+    def toggle_mute(self, clicked):
+
+        if self.isMuted:
+            self.isMuted = False
         else:
-            self.ui.mute.setText("Mute")
+            self.isMuted = True
         return self.isMuted
     #returns the status of isMuted, can change through main i guess
 
     # for toggling deafen
-    def toggle_deafen(self, checked):
-        self.isDeafened = checked
-        if checked:
-            self.ui.deafen.setText("Undeafen")
+    def toggle_deafen(self, clicked):
+        if self.isDeafened:
+            self.isDeafened = False;
         else:
-            self.ui.deafen.setText("Deafen")
+            self.isDeafened = True;
         return self.isDeafened
     #same idea as mute
 
@@ -99,12 +108,43 @@ class ui(QWidget):
             return self.sleep
         # is a clicky button that toggles sleep mode
 
+    def backEndBox(self, checked):
+        if checked:
+            self.ui.backendBox.show()
+        else:
+            self.ui.backendBox.hide()
+
+
     #constantly checks and updates the text on the sleep button
-    def update_sleep_button_text(self):
+    def update_buttons(self):
         if self.sleep:
             self.ui.sleepMode.setText("Wake")
+            img_path = os.path.join(os.path.dirname(__file__), "images/sleep.png")
+
         else:
             self.ui.sleepMode.setText("Sleep")
+            img_path = os.path.join(os.path.dirname(__file__), "images/awake.jpg")
+            
+        pixmap = QPixmap(img_path)
+        self.ui.sleepStatus.setScaledContents(True)
+        self.ui.sleepStatus.setPixmap(pixmap)
+
+        if self.isMuted:
+            self.ui.mute.setText("Unmute")
+
+        else:
+            self.ui.mute.setText("Mute")
+
+        
+
+        if self.isDeafened:
+            self.ui.deafen.setText("Undeafen")
+        else:
+            self.ui.deafen.setText("Deafen")
+
+
+
+
     
 #lowkey so like the variables are here we just have to integrate it into main 
 # then we can like write code to stop the listing or muting mic and deafening the sound and shii
--- UI/ui_form.py
+++ UI/ui_form.py
@@ -22,27 +22,149 @@ class Ui_ui(object):
     def setupUi(self, ui):
         if not ui.objectName():
             ui.setObjectName(u"ui")
-        ui.resize(800, 600)
-        self.deafen = QCheckBox(ui)
-        self.deafen.setObjectName(u"deafen")
-        self.deafen.setGeometry(QRect(520, 110, 75, 20))
-        self.mute = QCheckBox(ui)
-        self.mute.setObjectName(u"mute")
-        self.mute.setGeometry(QRect(520, 130, 75, 20))
+        ui.resize(1920, 1080)
+        ui.setMinimumSize(QSize(1920, 1080))
+        ui.setMaximumSize(QSize(1920, 1080))
+        font = QFont()
+        font.setFamilies([u"Cascadia Code"])
+        font.setPointSize(12)
+        ui.setFont(font)
+        ui.setAutoFillBackground(False)
+        ui.setStyleSheet(u"background: qlineargradient(x1:0, y1:0, x2:0, y2:1,\n"
+"    stop:0 #F5F5F5,\n"
+"    stop:1 #D3D3D3);\n"
+"color: #4B4B4B;\n"
+"border-radius: 6px;\n"
+"padding: 5px;")
         self.output = QLabel(ui)
         self.output.setObjectName(u"output")
-        self.output.setGeometry(QRect(30, 340, 461, 221))
+        self.output.setGeometry(QRect(10, 550, 630, 520))
+        font1 = QFont()
+        font1.setFamilies([u"Segoe UI"])
+        font1.setPointSize(12)
+        self.output.setFont(font1)
+        self.output.setAutoFillBackground(False)
+        self.output.setStyleSheet(u"background-color: #F8F8FF;  /* Light Gray */\n"
+"color: #000000;             /* Dark Gray text */\n"
+"border: 1px solid #C0C0C0;  /* Silver border */\n"
+"border-radius: 6px;\n"
+"padding: 5px;")
         self.output.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
         self.output.setWordWrap(True)
         self.input = QTextEdit(ui)
         self.input.setObjectName(u"input")
-        self.input.setGeometry(QRect(10, 30, 501, 271))
+        self.input.setGeometry(QRect(10, 10, 630, 520))
+        self.input.setFont(font1)
+        self.input.setAutoFillBackground(True)
+        self.input.setStyleSheet(u"background-color: #F8F8FF;  /* Light Gray */\n"
+"color: #000000;             /* Dark Gray text */\n"
+"border: 1px solid #C0C0C0;  /* Silver border */\n"
+"border-radius: 6px;\n"
+"padding: 5px;")
         self.enterButton = QPushButton(ui)
         self.enterButton.setObjectName(u"enterButton")
-        self.enterButton.setGeometry(QRect(530, 40, 75, 24))
+        self.enterButton.setGeometry(QRect(650, 10, 310, 50))
+        font2 = QFont()
+        font2.setFamilies([u"Cascadia Code"])
+        font2.setPointSize(30)
+        self.enterButton.setFont(font2)
+        self.enterButton.setStyleSheet(u"background-color: #F8F8FF;  /* Light Gray */\n"
+"color: #000000;             /* Dark Gray text */\n"
+"border: 1px solid #C0C0C0;  /* Silver border */\n"
+"border-radius: 6px;\n"
+"padding: 5px;")
         self.sleepMode = QPushButton(ui)
         self.sleepMode.setObjectName(u"sleepMode")
-        self.sleepMode.setGeometry(QRect(520, 150, 75, 24))
+        self.sleepMode.setGeometry(QRect(650, 190, 310, 50))
+        self.sleepMode.setFont(font2)
+        self.sleepMode.setAutoFillBackground(False)
+        self.sleepMode.setStyleSheet(u"background-color: #F8F8FF;  /* Light Gray */\n"
+"color: #000000;             /* Dark Gray text */\n"
+"border: 1px solid #C0C0C0;  /* Silver border */\n"
+"border-radius: 6px;\n"
+"padding: 5px;")
+        self.sleepStatus = QLabel(ui)
+        self.sleepStatus.setObjectName(u"sleepStatus")
+        self.sleepStatus.setGeometry(QRect(660, 250, 151, 151))
+        self.sleepStatus.setAutoFillBackground(False)
+        self.sleepStatus.setStyleSheet(u"background-color: #F8F8FF;  /* Light Gray */\n"
+"color: #000000;             /* Dark Gray text */\n"
+"border: 1px solid #C0C0C0;  /* Silver border */\n"
+"border-radius: 6px;\n"
+"padding: 5px;")
+        self.mute = QPushButton(ui)
+        self.mute.setObjectName(u"mute")
+        self.mute.setGeometry(QRect(650, 130, 310, 50))
+        self.mute.setFont(font2)
+        self.mute.setAutoFillBackground(False)
+        self.mute.setStyleSheet(u"background-color: #F8F8FF;  /* Light Gray */\n"
+"color: #000000;             /* Dark Gray text */\n"
+"border: 1px solid #C0C0C0;  /* Silver border */\n"
+"border-radius: 6px;\n"
+"padding: 5px;")
+        self.deafen = QPushButton(ui)
+        self.deafen.setObjectName(u"deafen")
+        self.deafen.setGeometry(QRect(650, 70, 310, 50))
+        font3 = QFont()
+        font3.setFamilies([u"Cascadia Code"])
+        font3.setPointSize(30)
+        font3.setBold(False)
+        self.deafen.setFont(font3)
+        self.deafen.setAutoFillBackground(False)
+        self.deafen.setStyleSheet(u"background-color: #F8F8FF;  /* Light Gray */\n"
+"color: #000000;             /* Dark Gray text */\n"
+"border: 1px solid #C0C0C0;  /* Silver border */\n"
+"border-radius: 6px;\n"
+"padding: 5px;")
+        self.backendBox = QLabel(ui)
+        self.backendBox.setObjectName(u"backendBox")
+        self.backendBox.setGeometry(QRect(1280, 59, 630, 1011))
+        self.backendBox.setMinimumSize(QSize(0, 6))
+        self.backendBox.setAutoFillBackground(False)
+        self.backendBox.setStyleSheet(u"background: qlineargradient(x1:0, y1:0, x2:0, y2:1,\n"
+"    stop:0 #252526,\n"
+"    stop:1 #1e1e1e);\n"
+"color: #d4d4d4;\n"
+"border-radius: 5px;\n"
+"padding: 6px;\n"
+"\n"
+"")
+        self.backendBox.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+        self.showBEBox = QCheckBox(ui)
+        self.showBEBox.setObjectName(u"showBEBox")
+        self.showBEBox.setGeometry(QRect(1280, 10, 461, 41))
+        font4 = QFont()
+        font4.setFamilies([u"Cascadia Code"])
+        self.showBEBox.setFont(font4)
+        self.showBEBox.setStyleSheet(u"QCheckBox {\n"
+"    spacing: 10px;\n"
+"    color: #4B4B4B;\n"
+"    font-size: 14px;\n"
+"}\n"
+"\n"
+"QCheckBox::indicator {\n"
+"    width: 20px;\n"
+"    height: 20px;\n"
+"    border-radius: 6px;\n"
+"    border: 1px solid #aaa;\n"
+"    background: qlineargradient(x1:0, y1:0, x2:0, y2:1,\n"
+"                                stop:0 #F5F5F5,\n"
+"                                stop:1 #D3D3D3);\n"
+"}\n"
+"\n"
+"/* Checked state */\n"
+"QCheckBox::indicator:checked {\n"
+"    background: qlineargradient(x1:0, y1:0, x2:0, y2:1,\n"
+"                                stop:0 #4B4B4B,\n"
+"                                stop:1 #2E2E2E);\n"
+"    image: url(:/icons/checkmark_white.svg);  /* Or use built-in checkmark if no image */\n"
+"}\n"
+"\n"
+"/* Optional: hover effect */\n"
+"QCheckBox::indicator:hover {\n"
+"    border: 1px solid #888;\n"
+"}")
+        self.showBEBox.setChecked(True)
 
         self.retranslateUi(ui)
 
@@ -51,10 +173,22 @@ class Ui_ui(object):
 
     def retranslateUi(self, ui):
         ui.setWindowTitle(QCoreApplication.translate("ui", u"ui", None))
-        self.deafen.setText(QCoreApplication.translate("ui", u"deafen", None))
-        self.mute.setText(QCoreApplication.translate("ui", u"mute", None))
-        self.output.setText(QCoreApplication.translate("ui", u"TextLabel", None))
-        self.enterButton.setText(QCoreApplication.translate("ui", u"OK", None))
+        self.output.setText(QCoreApplication.translate("ui", u"Output", None))
+        self.input.setHtml(QCoreApplication.translate("ui", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Segoe UI'; font-size:12pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Input</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
+        self.enterButton.setText(QCoreApplication.translate("ui", u"Enter", None))
         self.sleepMode.setText(QCoreApplication.translate("ui", u"Sleep", None))
+        self.sleepStatus.setText("")
+        self.mute.setText(QCoreApplication.translate("ui", u"Mute", None))
+        self.deafen.setText(QCoreApplication.translate("ui", u"Deafen", None))
+        self.backendBox.setText(QCoreApplication.translate("ui", u"TextLabel", None))
+        self.showBEBox.setText(QCoreApplication.translate("ui", u"Show Backend Box", None))
     # retranslateUi
 
